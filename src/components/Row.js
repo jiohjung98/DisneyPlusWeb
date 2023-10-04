fd04aabfbf232e443bc858fded7579f629cc5cbb
jiohjung98 <ckaquz98@naver.com>
2023-10-05 00:02:50 +0900

    Style: Row Component

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,5 +1,6 @@
  import React, { useCallback, useEffect, useState } from 'react'
  import axios from '../api/axios'
+ import "./Row.css";
  
  const Row = ({title, id, fetchUrl}) => {
 
@@ -20,20 +21,21 @@
    return (
      <div>
        <h2>{title}</h2>
-       <div className="silder">
+       <div className="slider">
         <div className="slider__arrow-left">
           <span className='arrow'>
             {"<"}
           </span>
         </div>
         <div id={id} className="row__posters">
-          {movies.map(movie => {
+          {movies.map(movie => (
              <img
              key={movie.id}
+             className='row__poster'
              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
              alt={movie.name}       
            />
-          })}
+          ))}
         </div>
           <div className="slider__arrow-right">
             <span className="arrow">
