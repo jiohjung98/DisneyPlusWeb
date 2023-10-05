da12aec003cc4b85ae211793a4a2bce415677d7f
jiohjung98 <ckaquz98@naver.com>
2023-10-05 13:12:50 +0900

    Feat: add movieModal

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,18 @@
- import React, { useCallback, useEffect, useState } from 'react'
- import axios from '../api/axios'
- import "./Row.css";
+import React, { useCallback, useEffect, useState } from 'react'
+import axios from '../api/axios'
+import "./Row.css";
+import MovieModal from './MovieModal';
  
  const Row = ({title, id, fetchUrl}) => {
 
   // 여러 개의 영화 정보가 들어갈 것이므로 객체가 아닌 배열로 넣어줌
   const [movies, setMovies] = useState([]);
+
+  // 영화 모달창
+  const [modalOpen, setModalOpen] = useState(false);
+
+  // 클릭한 영화 정보 가져오기
+  const [movieSelected, setMovieSelection] = useState({});
 
   // 컴포넌트가 랜더링되면 함수도 새로 생성됨. 이부분을 막기위해 useCallback 사용(함수를 메모이제이션해줌
   const fetchMovieData = useCallback(async () => {
@@ -18,7 +25,10 @@
     fetchMovieData();
   }, [fetchMovieData])
 
-  console.log(document.getElementById(id))
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelection(movie);
+  }
 
    return (
      <div>
@@ -40,7 +50,8 @@
              key={movie.id}
              className='row__poster'
              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-             alt={movie.name}       
+             alt={movie.name}  
+             onClick={() => handleClick(movie)}     
            />
           ))}
         </div>
@@ -55,6 +66,13 @@
             </span>
           </div>
        </div>
+
+       {modalOpen &&
+        <MovieModal
+          {...movieSelected}
+          setModalOpen={setModalOpen}
+        />
+        }  
      </div>
    )
  }
