c8bd6aed859e0e94aaaed0153673c4f8ee577cf8
jiohjung98 <ckaquz98@naver.com>
2023-10-06 22:30:16 +0900

    Create: movieDetailpage by useParams

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import axios from '../../api/axios'
 
 const DetailPage = () => {
+  let { movieId } = useParams();
+  // let movieId = useParams.movieId; 이를 줄여서 위처럼 사용
+
+
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+        const response = await axios.get(
+        `/movie/${movieId}`
+    )
+    setMovie(response.data)
+    }
+  fetchData();
+}, [movieId])
+
+  if (!movie) return null;
+
   return (
-    <div>
-      
-    </div>
+    <section>
+        <img
+        className="modal__poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt='img'
+        />
+    </section>
   )
 }
 
