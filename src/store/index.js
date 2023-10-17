381895340aaeb710e7483d871851223570b4ef12
jiohjung98 <ckaquz98@naver.com>
2023-10-17 19:55:41 +0900

    Feat: create redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from './userSlice'
+import storage from "redux-persist/lib/storage"
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+
+const rootReducer = combineReducers({
+    user: userReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer
-    }
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        }
+    }),
 })
+
+export const persistor = persistStore(store);
